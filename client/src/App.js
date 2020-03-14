9285b6d0cc384791874bd1450c210e83bcff504e
Andrew Haberman <a.haberman25@gmail.com>
2020-03-13 21:50:40 -0400

    update to not allow 2 titles of same name

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,22 @@ import SavedList from './Movies/SavedList';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {    
-    setSavedList( [...savedList, movie] );
+  const addToSavedList = movie => { 
+    let xx = savedList.map(savedInList => {
+      return savedInList.title
+    })
+    if (xx.includes(movie.title)) {
+      alert('cant add 2 movies of the same title')
+    } else {
+      setSavedList([...savedList, movie])
+    }    
   };
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path='/' component={MovieList} />
-      <Route path='/movies/:id' component={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
+      <Route path='/movies/:id' component={(props) => <Movie {...props} addToSavedList={addToSavedList} list={savedList} />} />
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -25,8 +25,8 @@ const Movie = (props) => {
   
   // Uncomment this only when you have moved on to the stretch goals
   const saveMovie = () => {
-    const addToSavedList = props.addToSavedList;
-    addToSavedList(movie)
+      const addToSavedList = props.addToSavedList;
+      addToSavedList(movie)
   }
 
   if (!movie) {
